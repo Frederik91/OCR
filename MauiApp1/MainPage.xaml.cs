8e65037c3496aef31a61bd1a6a003516a900838e
Frederik91 <frederik.t91@gmail.com>
2022-09-18 13:04:29 +0200

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
